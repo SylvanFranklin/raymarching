fad2e7728c9ffce31a2ebfb672430b42b5c72585
SylvanFranklin <sylvanfranklin@icloud.com>
2025-09-21 20:20:32 -0400

    remove thread

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,17 @@
 #include "engine.hpp"
 
-void render(){
-    Engine engine;
-    while (!engine.shouldClose()) {
-        engine.update();
-        engine.render();
-    }
-}
-
+// void render() {}
 
 int main(int argc, char *argv[]) {
 
-    std::thread renderThread(render);
-    renderThread.join();
+	// std::thread renderThread(render);
+	// renderThread.join();
+	Engine engine;
+	while (!engine.shouldClose()) {
+		engine.update();
+		engine.render();
+	}
 
 	glfwTerminate();
 	return 0;
 }
-
